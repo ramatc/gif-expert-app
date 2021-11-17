9ae526e39a45323e43b56548bc217853f5891e01
ramatc <rtanquiascornejo@gmail.com>
2021-11-17 17:15:08 -0300

    Corrigiendo errores

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import useFetchGifs from '../hooks/useFetchGifs';
 import GifGridItem from './GifGridItem';
-import not_results from '../assets/images/crying-cowbow-emoji.gif';
 
 const GifGrid = ({ category }) => {
     
@@ -17,7 +16,7 @@ const GifGrid = ({ category }) => {
                 images.length === 0 && 
                 <div className="container_not_found">
                     <h3 className="not_found">No GIFs found for your search</h3> 
-                    <img src={not_results} alt="No GIFs found for your search" className="img_results"></img>
+                    <img src="crying-cowbow-emoji.gif" alt="No GIFs found for your search" className="img_results"></img>
                 </div>
             }
 
