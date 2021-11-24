06f2532e80ea3427873d02ecea49dd988c418409
ramatc <rtanquiascornejo@gmail.com>
2021-11-24 13:55:41 -0300

    Modificando el input y el background

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    < GifExpertApp/>
+    <GifExpertApp/>
   </React.StrictMode>,
   document.getElementById('root')
 );
