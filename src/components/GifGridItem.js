74e00af52ea6861d1b5b9a75aec54b72401bfd3c
ramatc <rtanquiascornejo@gmail.com>
2022-02-25 23:57:27 -0300

    Modifico hover de la card

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -5,9 +5,10 @@ const GifGridItem = ( {title, url} ) => {
     return (
         <div className="card animate__animated animate__fadeIn">
             <a href={ url } target="_blank" rel="noreferrer noopener" className="link-img">
-                <img src={ url } alt={ title } />
-                <p> { title } </p> 
+                See Gif
             </a>
+            <img src={ url } alt={ title } />
+            <p> { title } </p> 
         </div>
     )
 }
