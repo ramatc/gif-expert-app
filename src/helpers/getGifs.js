8d4c22fecdbf691096aff87ae3674d40e27e4c7c
ramatc <rtanquiascornejo@gmail.com>
2023-07-12 21:36:13 -0300

    Agregando variables de entorno

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async( category ) => {
-    const APIKey = "o2jFEQeA5I0nwBWGLDuFJf2uKY2Zi1m8";
+    const APIKey = process.env.REACT_APP_API_KEY;
     const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=15&api_key=${APIKey}`;
     const resp = await fetch(url);
     const {data} = await resp.json();
